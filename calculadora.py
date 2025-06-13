56b12fb2a81ad3f1f5acc8f983208f75143cd1ca
Crifer1711 <cristian20182001@gmail.com>
2025-06-12 19:22:57 -0500

    Version 3 calculo del area del rectángulo

--- calculadora.py
+++ calculadora.py
@@ -4,10 +4,13 @@ def area_cuadrado(lado):
 def area_triangulo(base, altura):
     return (base * altura) / 2
 
+def area_rectangulo(base, altura):
+    return base * altura
+
 if __name__ == "__main__":
-    print("=== Calculadora de Área (Versión 2: Cuadrado + Triángulo) ===")
-    print("1. Cuadrado\n2. Triángulo")
-    opcion = int(input("Seleccione figura (1-2): "))
+    print("=== Calculadora de Área (Versión 3: Cuadrado + Triángulo + Rectángulo) ===")
+    print("1. Cuadrado\n2. Triángulo\n3. Rectángulo")
+    opcion = int(input("Seleccione figura (1-3): "))
 
     if opcion == 1:
         lado = float(input("Ingrese el lado del cuadrado: "))
@@ -16,5 +19,9 @@ if __name__ == "__main__":
         base = float(input("Ingrese la base del triángulo: "))
         altura = float(input("Ingrese la altura del triángulo: "))
         print(f"Área del triángulo: {area_triangulo(base, altura)}")
+    elif opcion == 3:
+        base = float(input("Ingrese la base del rectángulo: "))
+        altura = float(input("Ingrese la altura del rectángulo: "))
+        print(f"Área del rectángulo: {area_rectangulo(base, altura)}")
     else:
         print("Opción no válida")
