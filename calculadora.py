4eb70393f6cbc348fe403e3622f466bd32e60de2
Crifer1711 <cristian20182001@gmail.com>
2025-06-12 19:21:15 -0500

    Version 2 calculo área del triángulo

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,20 @@
 def area_cuadrado(lado):
     return lado * lado
 
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
 if __name__ == "__main__":
-    print("=== Calculadora de Área (Versión 1: Cuadrado) ===")
-    lado = float(input("Ingrese el lado del cuadrado: "))
-    print(f"El área del cuadrado es: {area_cuadrado(lado)}")
+    print("=== Calculadora de Área (Versión 2: Cuadrado + Triángulo) ===")
+    print("1. Cuadrado\n2. Triángulo")
+    opcion = int(input("Seleccione figura (1-2): "))
+
+    if opcion == 1:
+        lado = float(input("Ingrese el lado del cuadrado: "))
+        print(f"Área del cuadrado: {area_cuadrado(lado)}")
+    elif opcion == 2:
+        base = float(input("Ingrese la base del triángulo: "))
+        altura = float(input("Ingrese la altura del triángulo: "))
+        print(f"Área del triángulo: {area_triangulo(base, altura)}")
+    else:
+        print("Opción no válida")
